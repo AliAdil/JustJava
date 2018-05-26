0c52b3b8a393b276f11c1bb0229370c3a4b03b34
aliadil <aliadilg@Gmail.com>
2018-05-27 01:24:43 +0500

    update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        number += 1;
         quantityTextView.setText("" + number);
     }
 }
