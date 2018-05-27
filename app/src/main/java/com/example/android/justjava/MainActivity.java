4f31dc8fd1a36dc78d48ccfff5298330d391c401
aliadil <aliadilg@Gmail.com>
2018-05-28 03:20:18 +0500

    update

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,13 +25,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void increment(View view) {
         int quantity = 2;
-        quantity= 3;
+        quantity= quantity + 1;
+        quantity= quantity + 1;
         display(quantity);
 
     }
     public void decrement(View view) {
         int quantity = 2;
-        quantity = 1;
+        quantity = quantity- 1;
         display(quantity);
 
     }
